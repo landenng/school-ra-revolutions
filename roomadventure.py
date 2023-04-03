ebe415f5d298424c2030aacae8863c490f8221c5
landennguyen <landenttvnguyen@gmail.com>
2023-04-03 14:37:37 -0500

    added room class

--- roomadventure.py
+++ roomadventure.py
@@ -6,7 +6,45 @@ from tkinter import *
 
 
 class Room:
-    pass
+    """A Room has a name and a file path that points to a .gif image."""
+
+    def __init__(self, name: str, image_filepath: str) -> None:
+        self.name = name
+        self.image = image_filepath
+        self.exits = {}
+        self.items = {}
+        self.grabbables = []
+
+    # skipping getters and setters
+    def add_exit(self, label: str, room: "Room"):
+        self.exits[label] = room
+
+    def add_item(self, label: str, desc: str):
+        self.items[label] = desc
+
+    def add_grabbables(self, label: str):
+        self.grabbables.append(label)
+
+    def del_grabbables(self, label: str):
+        self.grabbables.remove(label)
+
+    def __str__(self) -> str:
+        # create base response
+        result = f"You are in {self.name}\n"
+
+        # append the items you see in the room
+        result += f"You see: "
+        for item in self.items.keys():
+            result += item + " "
+        result += "\n"
+
+        # append the exits available
+        result += "Exits: "
+        for exit in self.exits.keys():
+            result += exit + " "
+        result += "\n"
+
+        return result
 
 class Game:
     pass
